1851b80ce74e6c2608a804caf961d140f54d9a38
il421 <il421@mail.ru>
2019-06-15 00:55:53 +1200

    Innitial commit

--- src/tests/components/DashboardPage.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import Booking from '../../components/Booking';
-
-test('should render Booking', () => {
-  const wrapper = shallow(<Booking />);
-  expect(wrapper).toMatchSnapshot();
-});
--- src/tests/setupTests.js
+++ src/tests/setupTests.js
@@ -1,11 +1,6 @@
 import Enzyme from 'enzyme';
 import Adaptor from 'enzyme-adapter-react-16';
-import DotEnv from 'dotenv';
 
 Enzyme.configure({
   adapter: new Adaptor()
-});
-
-DotEnv.config({
-  path: '.env.test'
 });
